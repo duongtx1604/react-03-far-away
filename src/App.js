12ed6a897aef8b02a8c741e3306cd446ccab4141
Duong Truong <91935964+duongtx1604@users.noreply.github.com>
2023-12-22 22:08:23 +0900

    update stats

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
                 onDeleteItem={handleDeleteItem}
                 onPacked={handlePacked}
             ></PackingList>
-            <Starts></Starts>
+            <Starts items={items}></Starts>
         </div>
     );
 }
@@ -116,11 +116,23 @@ function Item({ item, onDeleteItem, onPacked }) {
     );
 }
 
-function Starts() {
+function Starts({ items }) {
+    if (!items.length)
+        return (
+            <footer className="stats">
+                <em>Start adding some items to your packing list...</em>
+            </footer>
+        );
+
+    const numItems = items.length;
+    const packed = items.filter((item) => item.packed).length;
+    const per = Math.round((packed / numItems) * 100);
     return (
         <footer className="stats">
             <em>
-                You have X items on your list, and you already packed X (X%)
+                {per === 100
+                    ? "You got everything! Ready to go ✈️✈️"
+                    : `You have ${numItems} items on your list, and you already packed ${packed} (${per}%)`}
             </em>
         </footer>
     );
